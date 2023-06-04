0a9660c8f4736e295bfba49a85b123a2f20fe1bb
Volodymyr Vitiuk <122730435+VitiukVV@users.noreply.github.com>
2023-06-04 12:59:35 +0300

    mod-3

---
+++ index.js
@@ -0,0 +1,517 @@
+// // console.log('Hello World')
+// const playList = {
+//     name: [1, 2, 3],
+// };
+// console.log(playList)
+//
+// const apartment = {
+//     imgUrl: "https://via.placeholder.com/640x480",
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//     tags: ["premium", "promoted", "top"],
+//     owner: {
+//       name: "Henry",
+//       phone: "982-126-1588",
+//       email: "henry.carter@aptmail.com",
+//     },
+//   };
+
+//   // Change code below this line
+//   const ownerName = apartment.owner.name;
+//   const ownerPhone = apartment.owner.phone;
+//   const ownerEmail = apartment.owner.email;
+//   const numberOfTags = apartment.tags.length;
+//   const firstTag = apartment.tags[0];
+//   const lastTag = apartment.tags[numberOfTags - 1];
+//   // Change code above this line
+//   console.log(lastTag)
+//
+// const apartment = {
+//     imgUrl: "https://via.placeholder.com/640x480",
+//     descr: "Spacious apartment in the city center",
+//     rating: 4.7,
+//     price: 5000,
+//     tags: ["premium", "promoted", "top", "trusted"],
+//     owner: {
+//       name: "Henry Sibola",
+//       phone: "982-126-1588",
+//       email: "henry.carter@aptmail.com",
+//     },
+//   };
+
+//   // Change code below this line
+//     apartment.area = 60;
+//     apartment.rooms = 3;
+//     apartment.location = {
+//       country: "Jamaica",
+//       city: "Kingston"}
+
+//       console.log(apartment.location)
+//
+// const apartment = {
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//   };
+//   const keys = [];
+//   const values = [];
+//   // Change code below this line
+//     for (const key in apartment) {
+//       keys.push(key);
+//       values.push(apartment[key])
+//     }
+//     console.log(keys)
+//     console.log(values)
+// //
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: "apt",
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = "Spacious apartment in the city center";
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   // Change code below this line
+//     if (apartment.hasOwnProperty(key)) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+//     }
+//   // Change code above this line
+// }
+// console.log(apartment)
+//
+// function countProps(object) {
+//     let propCount = 0;
+//     // Change code below this line
+//     for (const key in object) {
+//         if (object.hasOwnProperty(key)) {
+//             propCount += 1;
+//         }
+//     }
+//     // Change code above this line
+//     return propCount;
+//   }
+//   console.log(countProps({}))
+//   console.log(countProps({ name: "Mango", age: 2 }))
+//   console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }))
+//
+// const apartment = {
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//   };
+//   const values = [];
+//   // Change code below this line
+//   const keys = Object.keys(apartment);
+//   for (const key of keys) {
+//     values.push(apartment[key])
+//   }
+//   console.log(keys)
+//   console.log(values)
+//
+// function countProps(object) {
+//     // Change code below this line
+//     let propCount = Object.keys(object);
+//     // console.log(propCount)
+//     return propCount.length;
+//     // Change code above this line
+//   }
+//   console.log(countProps({}))
+//   console.log(countProps({ name: "Mango", age: 2 }))
+//   console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }))
+//
+// const apartment = {
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//   };
+//   // Change code below this line
+//   const keys = Object.keys(apartment);
+//   const values = Object.values(apartment);
+//   console.log(keys)
+//   console.log(values)
+//
+// function countTotalSalary(salaries) {
+//     let totalSalary = 0;
+//     // Change code below this line
+//     const values = Object.values(salaries);
+//     for (const value of values) {
+//         totalSalary += value
+//     }
+//     // Change code above this line
+//     return totalSalary;
+//     }
+// console.log(countTotalSalary({}))
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }))
+// console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }))
+//
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (const color of colors) {
+//   hexColors.push(color.hex)
+//   rgbColors.push(color.rgb)
+// }
+// console.log(hexColors)
+// console.log(rgbColors)
+//
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   for (const product of products) {
+//     if (productName === product.name) {
+//       return product.price
+//     }
+//   }
+//   return null
+
+//   // Change code above this line
+// }
+// console.log(getProductPrice("Radar"))
+// console.log(getProductPrice("Grip"))
+// console.log(getProductPrice("Scanner"))
+// console.log(getProductPrice("Droid"))
+// console.log(getProductPrice("Engine"))
+//
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   let value = [];
+//   for (const product of products) {
+//     // console.log(product);
+//     const keys = Object.keys(product);
+//     // console.log(Object.keys(product));
+//     for (const key of keys) {
+//       // console.log(key);
+//       if (propName === key) {
+//         value.push(product[propName]);
+//       }
+//     }
+//   }
+//   return value;
+
+//   // Change code above this line
+// }
+// console.log(getAllPropValues("name"));
+// console.log(getAllPropValues("quantity"));
+// console.log(getAllPropValues("price"));
+// console.log(getAllPropValues("category"));
+//
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   let value = [];
+//   for (const product of products) {
+//     if (Object.keys(product).includes(propName)) {
+//       value.push(product[propName]);
+//     }
+//   }
+//   return value;
+
+//   // Change code above this line
+// }
+// console.log(getAllPropValues("name"));
+// console.log(getAllPropValues("quantity"));
+// console.log(getAllPropValues("price"));
+// console.log(getAllPropValues("category"));
+//
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Change code below this line
+//   totalCost = 0;
+//   for (const product of products) {
+//     if (Object.values(product).includes(productName)) {
+//       totalCost = product.price * product.quantity;
+//     }
+//   }
+//   return totalCost;
+//   // Change code above this line
+// }
+// console.log(calculateTotalPrice("Blaster"));
+// console.log(calculateTotalPrice("Radar"));
+// console.log(calculateTotalPrice("Droid"));
+// console.log(calculateTotalPrice("Grip"));
+// console.log(calculateTotalPrice("Scanner"));
+//
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Change code below this line
+//   totalCost = 0;
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       totalCost = product.price * product.quantity;
+//     }
+//   }
+//   return totalCost;
+//   // Change code above this line
+// }
+// console.log(calculateTotalPrice("Blaster"));
+// console.log(calculateTotalPrice("Radar"));
+// console.log(calculateTotalPrice("Droid"));
+// console.log(calculateTotalPrice("Grip"));
+// console.log(calculateTotalPrice("Scanner"));
+//
+
+// У прогнозі максимальних температур також може бути необов'язкова властивість icon - іконка погоди. Заміни оголошення змінних yesterday, today, tomorrow і icon однією операцією деструктуризації властивостей об'єкта highTemperatures. Задай значення за замовчуванням для icon - рядок "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg".
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+// const {
+//   yesterday,
+//   today,
+//   tomorrow,
+//   icon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+// } = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+//
+/* task 23 */
+// Заміни оголошення змінних highYesterday, highToday, highTomorrow і highIcon однією операцією деструктуризації властивостей об'єкта highTemperatures. Задай значення за замовчуванням для highIcon - рядок "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg".
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+// const {
+//   yesterday: highYesterday,
+//   today: highToday,
+//   tomorrow: highTomorrow,
+//   icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+// } = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+//
+// task 25
+/* Ми отримали прогноз погоди на два дні, з мінімальними і максимальними температурами, а також необов'язковими іконками. Заміни оголошення всіх змінних однією операцією деструктуризації властивостей об'єкта forecast. Задай значення за замовчуванням для іконок, змінних todayIcon і tomorrowIcon - рядок "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg". */
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+// // Change code below this line
+// const {
+//   today: {
+//     low: lowToday,
+//     high: highToday,
+//     icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+//   },
+//   tomorrow: {
+//     low: lowTomorrow,
+//     high: highTomorrow,
+//     icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+//   },
+// } = forecast;
+//
+//
+/* task 26 */
+// Change code below this line
+// function calculateMeanTemperature(forecast) {
+//   const {
+//     today: { low: todayLow, high: todayHigh },
+//     tomorrow: { low: tomorrowLow, high: tomorrowHigh },
+//   } = forecast;
+
+//   // Change code above this line
+//   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+// }
+// console.log(
+//   calculateMeanTemperature({
+//     today: { low: 28, high: 32 },
+//     tomorrow: { low: 25, high: 29 },
+//   })
+// );
+//
+//
+// task 27
+/* У змінній scores зберігається масив результатів тестування. Використовуючи розподіл і методи Math.max() і Math.min(), доповни код таким чином, щоб у змінній bestScore був найвищий бал, а у worstScore - найнижчий. */
+// const scores = [89, 64, 42, 17, 93, 51, 26];
+// // Change code below this line
+// const bestScore = Math.max(...scores);
+// const worstScore = Math.min(...scores);
+// console.log(bestScore);
+// console.log(worstScore);
+//
+//
+/* task 28 */
+/* У змінних firstGroupScores, secondGroupScores і thirdGroupScores зберігаються результати тестування окремих груп. Використовуючи розподіл, доповни код таким чином, щоб:
+
+У змінній allScores зберігався масив всіх результатів від першої до третьої групи.
+У змінній bestScore був найвищий загальний бал.
+У змінній worstScore був найнижчий загальний бал. */
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+// // Change code below this line
+// const allScores = [
+//   ...firstGroupScores,
+//   ...secondGroupScores,
+//   ...thirdGroupScores,
+// ];
+// const bestScore = Math.max(...allScores);
+// const worstScore = Math.min(...allScores);
+// console.log(bestScore);
+// console.log(worstScore);
+//
+//
+/* task 29 */
+// const defaultSettings = {
+//   theme: "light",
+//   public: true,
+//   withPassword: false,
+//   minNumberOfQuestions: 10,
+//   timePerQuestion: 60,
+// };
+// const overrideSettings = {
+//   public: false,
+//   withPassword: true,
+//   timePerQuestion: 30,
+// };
+// // Change code below this line
+// const finalSettings = { ...defaultSettings, ...overrideSettings };
+// console.log(finalSettings);
+//
+//
+/* task 30 */
+// function makeTask(data) {
+//   const completed = false;
+//   const category = "General";
+//   const priority = "Normal";
+//   // Change code below this line
+//   return { completed, category, priority, ...data };
+//   // Change code above this line
+// }
+// console.log(makeTask({}));
+// console.log(
+//   makeTask({
+//     category: "Homemade",
+//     priority: "Low",
+//     text: "Take out the trash",
+//   })
+// );
+//
+//
+/* task 31 */
+/* Використовуючи операцію rest, доповни код функції add() таким чином, щоб вона приймала будь-яку кількість аргументів, рахувала і повертала їх суму. */
+// Change code below this line
+// function add(...args) {
+//   let total = 0;
+//   for (const arg of args) {
+//     total += arg;
+//   }
+//   return total;
+//   // Change code above this line
+// }
+//
+//
+/* task 32 */
+// Change code below this line
+// function addOverNum(firstArg, ...args) {
+//   let total = 0;
+
+//   for (const arg of args) {
+//     if (arg > firstArg) {
+//       total += arg;
+//     }
+//   }
+
+//   return total;
+//   // Change code above this line
+// }
+
+// console.log(addOverNum(50, 15, 27));
+// console.log(addOverNum(10, 12, 4, 11, 48, 10, 8));
+//
+//
+/* task 33 */
+/* Функція findMatches() приймає довільну кількість аргументів. Першим аргументом завжди буде масив чисел, а решта аргументів будуть просто числами.
+
+Доповни код функції таким чином, щоб вона повертала новий масив matches, в якому будуть тільки ті аргументи, починаючи з другого, які є в масиві першого аргументу.
+
+Наприклад, findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7) повинна повернути масив [1, 2], тому що тільки вони є в масиві першого аргументу. */
+// // Change code below this line
+// function findMatches(firstArg, ...args) {
+//   const matches = []; // Don't change this line
+//   for (const arg of firstArg) {
+//     console.log(arg);
+//     if (args.includes(arg)) {
+//       matches.push(arg);
+//     }
+//   }
+//   console.log(args);
+//   // Change code above this line
+//   return matches;
+// }
+// console.log(findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7));
+//
+const firstBook = {
+  title: "The Last Kingdom",
+  coverImage:
+    "https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg",
+};
+
+const {
+  title: firstTitle,
+  coverImage: firstCoverImage = "https://via.placeholder.com/640/480",
+} = firstBook;
+
+console.log(firstTitle); // The Last Kingdom
+console.log(firstCoverImage); // https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg
